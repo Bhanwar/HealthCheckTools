a2999b610d08e20bba79aabd3f5f8d0f812917ba
Rahul Rathi <rahul.rathi@snapdeal.com>
2016-05-17 16:33:55 +0530

    SNAPDEALTECH-66895:Bug fixes

---
+++ HealthCheckApp/src/main/java/com/snapdeal/healthcheck/app/model/HttpCallResponse.java
@@ -0,0 +1,37 @@
+package com.snapdeal.healthcheck.app.model;
+
+public class HttpCallResponse {
+
+	String statusCode;
+	String responseBody;
+	String httpCallException;
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getResponseBody() {
+		return responseBody;
+	}
+
+	public void setResponseBody(String responseBody) {
+		this.responseBody = responseBody;
+	}
+
+	public String getHttpCallException() {
+		return httpCallException;
+	}
+
+	public void setHttpCallException(String httpCallException) {
+		this.httpCallException = httpCallException;
+	}
+
+	@Override
+	public String toString() {
+		return "HttpCallResponse [statusCode=" + statusCode + ", responseBody=" + responseBody + "]";
+	}	
+}
